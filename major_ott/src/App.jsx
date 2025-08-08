4ada88b2a08969f101be24e463b1d3ef04531dcc
satyamkumar9-singh <satyamkumar2559@gmail.com>
2025-08-08 12:01:52 +0530

    8aug

---
+++ major_ott/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const notify =()=>Toast("wow so easy")
+  return (
+    export default function App() {
+  return (
+    <>
+    
+    </>
+  )
+}
+  );
+}
+
+export default App;
